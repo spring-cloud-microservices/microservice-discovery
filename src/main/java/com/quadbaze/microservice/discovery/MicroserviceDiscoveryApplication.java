0c2b2f92aa4ffeaa386858cc3eaf9d6df385d3cf
Olatunji Longe <olatunji.longe@gmail.com>
2018-08-31 00:13:58 -0400

    implemented feign / circuit-breaker

--- src/main/java/com/quadbaze/microservice/discovery/MicroserviceDiscoveryApplication.java
+++ src/main/java/com/quadbaze/microservice/discovery/MicroserviceDiscoveryApplication.java
@@ -4,16 +4,19 @@ import com.quadbaze.microservice.discovery.browser.BrowserUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class MicroserviceDiscoveryApplication {
 
 	public static void main(String[] args) {
-		BrowserUtil.browse(
-				SpringApplication.run(MicroserviceDiscoveryApplication.class, args)
-						.getEnvironment()
-						.getProperty("discovery.server.uri")
-		);
+		ConfigurableApplicationContext context = SpringApplication.run(MicroserviceDiscoveryApplication.class, args);
+
+		//Launch home-page to browser if environment is dev
+		if(context.getEnvironment().getProperty("spring.profiles.active").equals("dev")){
+			BrowserUtil.browse(context.getEnvironment().getProperty("discovery.server.uri"));
+		}
+
 	}
 }
